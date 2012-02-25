1b18e6aca66d1ded1690fc8c14f2f167999e1d5f
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-02-25 00:30:56 -0800

    cleanup a bunch of shit

--- docdrop.py
+++ docdrop.py
@@ -12,12 +12,16 @@ urls = (
 class main:
 	def GET(self):
 		return pystache.render(open('docdrop.html').read(), {})
+	
 	def POST(self):
 		formInput = web.input(docableFile={})
+		# If we get the file via AJAX, it doesn't fill in the filename and such
+		# properly.  Instead, the first part of the file contents is used as
+		# the key. *shrug*
 		if formInput['docableFile'] == {}:
+			# pycco barfs if there's no file extension...
 			filename = 'unknown.unknown'
 			code = formInput.keys()[0] + formInput[formInput.keys()[0]]
-			#import ipdb; ipdb.set_trace()
 		else:
 			filename = formInput['docableFile'].filename
 			code = formInput['docableFile'].value
--- static/docdrop.js
+++ static/docdrop.js
@@ -1,45 +1,12 @@
 window.onload = function() {
-	/*
-	var holder = document.getElementById('holder');
-	holder.ondragover = function() { return false; };
-	holder.ondragenter = function() { return false; };
-	holder.ondrop = function(e) {
-		e = e || window.event;
-		
-		// Read from e.files, as well as e.dataTransfer
-		var files = (e.files || e.dataTransfer.files);
-		
-		var s = "";
-		for (var i = 0; i < files.length; i++) {
-			(function (i) {
-				var reader = new FileReader();
-				reader.onload = function (event) {
-					holder.innerHTML = "<li><img src='" + event.target.result + "' /> " + (files[i].name) + "</li>" + holder.innerHTML;
-				};
-				reader.readAsDataURL(files[i]);
-			})(i);
-		}
-
-		return false;
-	};
-	*/
-	
 	var fileselect = document.getElementById('docableFile');
 	var filedrag = document.getElementById('holder');
-	var submitbutton = document.getElementById('submit');
-	
-	//fileselect.addEventListener('change', FileSelectHandler, false);
 	
 	var xhr = new XMLHttpRequest();
 	if (xhr.upload) {
 		filedrag.addEventListener('dragover', FileDragHover, false);
 		filedrag.addEventListener('dragleave', FileDragHover, false);
 		filedrag.addEventListener('drop', FileSelectHandler, false);
-	}
-	
-	function Output(msg) {
-		var m = document.getElementById("messages");
-		m.innerHTML = msg + m.innerHTML;
 	}
 	
 	function FileDragHover(e) {
@@ -53,23 +20,14 @@ window.onload = function() {
 		
 		var files = e.target.files || e.dataTransfer.files;
 		for (var i=0, f; f=files[i]; i++) {
-			ParseFile(f);
 			UploadFile(f);
 		}
-	}
-	
-	function ParseFile(file) {
-		Output(
-			"<p>File information: <strong>" + file.name +
-			"</strong> type: <strong>" + file.type +
-			"</strong> size: <strong>" + file.size +
-			"</strong> bytes</p>"
-		);
 	}
 	
 	function UploadFile(file) {
 		var xhr = new XMLHttpRequest();
 		xhr.open('POST', document.getElementById('upload').action, false);
+		// This doesn't seem to actually work.
 		xhr.setRequestHeader('X_FILENAME', file.name);
 		xhr.send(file);
 		document.body.innerHTML = xhr.responseText;
