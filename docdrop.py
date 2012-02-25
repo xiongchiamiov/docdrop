7d19938827cbb7fe9e1a07487784b3b142d81dbd
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-02-25 00:54:05 -0800

    add sqlite-inspired benediction

--- docdrop.py
+++ docdrop.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2
 
+# May you recognize your weaknesses and share your strengths.
+# May you share freely, never taking more than you give.
+# May you find love and love everyone you find.
+
 import pystache
 import web
 
